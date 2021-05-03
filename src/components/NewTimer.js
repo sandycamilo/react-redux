20b735160b4a50bcb5d877250a867fe35b319116
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2021-05-03 00:35:14 -0700

    added new timer input

---
+++ src/components/NewTimer.js
@@ -0,0 +1,24 @@
+// used to create a value that will be available across multiple renders of this component
+import React, { useState } from 'react'
+// access to the Redux store to allow component to send actions when updating store
+import { useDispatch } from 'react-redux'
+import { addTimer } from '../actions'
+
+export default function NewTimer() {
+  const [ name, setName ] = useState('')
+  const dispatch = useDispatch()
+  return (
+    <div>
+      <input
+        type='text'
+        placeholder='New Timer Name'
+        name='name'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        />
+      <button
+        onClick={() => dispatch(addTimer(name))}
+      >Save</button>
+    </div>
+  )
+}
