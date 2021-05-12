5cb9ac08a282a8b8d6c57bd99959cd37e98d0311
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2021-05-12 00:01:14 -0700

    Updates

--- src/Timer.js
+++
@@ -1,9 +0,0 @@
-class Timer {
-  constructor(name) {
-    this.name = name;
-    this.time = 0;
-    this.isRuninng = false
-  }
-}
-
-export default Timer;
--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import timerReducer from './timersReducer';
-import selectedTimeReducer from './selectedTimeReducer';
-
-export default combineReducers({
-  timers: timerReducer,              // array
-  selectedTimer: selectedTimeReducer, // int/number
-});
